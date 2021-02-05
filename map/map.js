8fd11f21698b79d1da147f928f0cf87d57f53794
Gustavo Allfadir <gustavo.allfadir@gmail.com>
2021-02-04 19:54:30 -0600

    initial

---
+++ map/map.js
@@ -0,0 +1,9 @@
+function main(list) {
+    list.map(function(element) {
+        return console.log(element);
+    });
+}
+
+main(['Hello','my','friends.']);
+main(['This','is','another','list.']);
+main(['Function','is being','excecuted','for every','element','of the list.']);
---
+++ readme.md
@@ -0,0 +1,3 @@
+# JavaScript excercises
+
+This repository is just a place to upload JavaScript/Node.js excercises and challenges.
